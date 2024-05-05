b9f0d4fb034665fc460d51e9fe1df0eb21f54022
mussab_tafal <mussab_tafal@hotmail.com>
2024-05-05 17:07:38 +0400

    Finishing experience section.

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,131 @@
 import React from "react";
 import "../experience/experience.css";
+import { LiaReact } from "react-icons/lia";
+import { FaAngular } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaBootstrap } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { IoLogoCss3 } from "react-icons/io";
+import { IoLogoPython } from "react-icons/io5";
+import { TbBrandMysql } from "react-icons/tb";
+import { TbBrandMongodb } from "react-icons/tb";
+import { IoLogoNodejs } from "react-icons/io5";
+import { FaHashtag } from "react-icons/fa";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="cotainer experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <FaHtml5 className="experience__details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoLogoCss3 className="experience__details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoLogoJavascript className="experience__details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaBootstrap className="experience__details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <RiTailwindCssFill className="experience__details-icon" />
+              <div>
+                <h4>Tailwind</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaAngular className="experience__details-icon" />
+              <div>
+                <h4>Angular</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <LiaReact className="experience__details-icon" />
+              <div>
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND */}
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <IoLogoNodejs className="experience__details-icon" />
+              <div>
+                <h4>NodeJs</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <FaHashtag
+                className="experience__details-icon"
+                style={{ marginRight: "0.5rem" }}
+              />
+              <div>
+                <h4>C#</h4>
+                <p>ASP.net</p>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <IoLogoPython
+                className="experience__details-icon"
+                style={{ marginRight: "0.5rem" }}
+              />
+              <div style={{ marginRight: "1.5rem" }}>
+                <h4>Python</h4>
+                <p>Django</p>
+                <small className="text-light">Basic</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <TbBrandMysql className="experience__details-icon" />
+              <div>
+                <h4>MySQL</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <TbBrandMongodb className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
