89ade16de10e1a248b2322633d477d860f790831
mussab_tafal <mussab_tafal@hotmail.com>
2024-05-09 00:59:38 +0400

    Finalizing services.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,7 +8,6 @@ import IMG2 from "../../assets/portfolio2.jpg";
 import IMG3 from "../../assets/portfolio3.jpg";
 import IMG4 from "../../assets/portfolio4.jpg";
 import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.jpg";
 
 const Portfolio = () => {
   return (
@@ -17,12 +16,50 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <PortfolioCard image={IMG1} title={"JobPal"} technologies={["Python", "Django", "HTML", "CSS", "JavaScript"]} github={"https://github.com/mussabtafal/Jobpal_Project"} link={"https://www.jobpal.work"}/>
-        <PortfolioCard image={IMG1} title={"OmniFoods"} technologies={["HTML", "CSS", "JavaScript", "nodeJS", "MongoDB"]}  github={"https://github.com/mussabtafal/OmniFoods_Project"} link={"https://omnifood-mussab.netlify.app/"}/>
-        <PortfolioCard image={IMG1} title={"O-Connect"} technologies={["React", "nodeJS", "HTML", "CSS", "JavaScript"]}  github={"https://www.onpassive.com/"} link={"https://www.onpassive.com/"}/>
-        <PortfolioCard image={IMG1} title={"Aerdata Asset Management Solution"} technologies={["C#", "ASP.net", "Angular", "JavaScript", "SQL Server"]}  github={"#"} link={"#"} disabled/>
-        <PortfolioCard image={IMG1} title={"Doctors Commuinity"} technologies={["Java", "Spring boot", "HTML", "CSS", "JavaScript"]}  github={"www.google.com"} link={"www.google.com"}/>
-        <FaPlus className="plus-icon"/>
+        <PortfolioCard
+          image={IMG1}
+          title={"JobPal"}
+          technologies={["Python", "Django", "HTML", "CSS", "JavaScript"]}
+          github={"https://github.com/mussabtafal/Jobpal_Project"}
+          link={"https://www.jobpal.work"}
+        />
+        <PortfolioCard
+          image={IMG2}
+          title={"OmniFoods"}
+          technologies={["HTML", "CSS", "JavaScript", "nodeJS", "MongoDB"]}
+          github={"https://github.com/mussabtafal/OmniFoods_Project"}
+          link={"https://omnifood-mussab.netlify.app/"}
+        />
+        <PortfolioCard
+          image={IMG3}
+          title={"O-Connect"}
+          technologies={["React", "nodeJS", "HTML", "CSS", "JavaScript"]}
+          github={"https://www.onpassive.com/"}
+          link={"https://www.onpassive.com/"}
+        />
+        <PortfolioCard
+          image={IMG4}
+          title={"Aerdata Asset Management Solution"}
+          technologies={[
+            "C#",
+            "ASP.net",
+            "Angular",
+            "JavaScript",
+            "SQL Server",
+          ]}
+          github={"#"}
+          link={"#"}
+          disabled
+        />
+        <PortfolioCard
+          image={IMG5}
+          title={"Doctors Commuinity"}
+          technologies={["Java", "Spring boot", "HTML", "CSS", "JavaScript"]}
+          github={"https://github.com/mussabtafal/DoctorsCommunity_Project"}
+          link="#"
+          linkDisabled
+        />
+        <FaPlus className="plus-icon" />
       </div>
     </section>
   );
--- src/components/portfolio/components/PortfolioCard.jsx
+++ src/components/portfolio/components/PortfolioCard.jsx
@@ -1,34 +1,46 @@
-import React from 'react'
+import React from "react";
 import { FaLock } from "react-icons/fa";
 
-const PortfolioCard = ({image, title, github, link, technologies, disabled=false}) => {
+const PortfolioCard = ({
+  image,
+  title,
+  github,
+  link,
+  technologies,
+  linkDisabled = false,
+  disabled = false,
+}) => {
+  const clickableBtn = linkDisabled ? " unclickable" : "";
+
   return (
     <article className="portfolio__item">
-    <div className="portfolio__item-image">
-      <img src={image} alt="" />
-    </div>
-    <h3>{title}</h3>
-    <ul className="technologies">
-      {technologies.map((tech) => {
-        return <li># {tech}</li>
-      })}
-    </ul>
-    {!disabled ?
-    <div className="portfolio__item-cta">
-      <a href={github} className="btn">
-        Github
-      </a>
-      <a
-        href={link}
-        target="_blank"
-        className="btn btn-primary"
-      >
-        Live Demo
-      </a>
-    </div>: <FaLock className='lock-icon'/>
-}
-  </article>
-  )
-}
+      <div className="portfolio__item-image">
+        <img src={image} alt="" />
+      </div>
+      <h3>{title}</h3>
+      <ul className="technologies">
+        {technologies.map((tech) => {
+          return <li># {tech}</li>;
+        })}
+      </ul>
+      {!disabled ? (
+        <div className="portfolio__item-cta">
+          <a href={github} className="btn">
+            Github
+          </a>
+          <a
+            href={link}
+            target="_blank"
+            className={"btn btn-primary" + clickableBtn}
+          >
+            Live Demo
+          </a>
+        </div>
+      ) : (
+        <FaLock className="lock-icon" />
+      )}
+    </article>
+  );
+};
 
-export default PortfolioCard
+export default PortfolioCard;
