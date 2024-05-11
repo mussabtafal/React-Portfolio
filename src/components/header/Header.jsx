755a67f6524b86d7da77f0b59b0ada46ea8b67be
mussab_tafal <mussab_tafal@hotmail.com>
2024-05-11 16:15:16 +0400

    re-styling header component.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "../header/header.css";
 import CTA from "../header/CTA";
-import ME from "../../assets/me.png";
 import HeaderSocials from "./HeaderSocials";
 import { TypeAnimation } from "react-type-animation";
 
@@ -9,29 +8,30 @@ const Header = () => {
   return (
     <header>
       <div className="container header_container">
-        <h5>Hello I'm </h5>
-        <h1>Mussab A. Taffal</h1>
-        <TypeAnimation
-          sequence={[
-            "Fullstack Developer",
-            2000,
-            "Civil Engineer",
-            2000,
-            "Designer",
-            2000,
-          ]}
-          wrapper="span"
-          speed={50}
-          className="text-light"
-          // style={{ fontSize: "1em", display: "inline-block" }}
-          repeat={Infinity}
-        />
-        <CTA />
-        <HeaderSocials />
-
-        <div className="me">
-          <img src={ME} alt="me" />
+        <div className="main">
+          <div>
+            <h5>Hello 👋, I'm </h5>
+            <h1>Mussab A. Taffal</h1>
+            <TypeAnimation
+              sequence={[
+                "Fullstack Developer",
+                2000,
+                "Civil Engineer",
+                2000,
+                "Designer",
+                2000,
+              ]}
+              wrapper="span"
+              speed={50}
+              className="text-light"
+              repeat={Infinity}
+            />
+            <CTA />
+          </div>
+          <div className="me">{/* <img src={ME} alt="me" /> */}</div>
         </div>
+
+        <HeaderSocials />
 
         <a href="#contact" className="scroll_down">
           Scroll Down
