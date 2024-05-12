d69c67cad31c816065ca80341f80c94cff9ce6ba
mussab_tafal <mussab_tafal@hotmail.com>
2024-05-12 18:15:34 +0400

    Putting the journey section initial structure.

--- src/components/journey/Journey.jsx
+++ src/components/journey/Journey.jsx
@@ -1,11 +1,163 @@
 import React from "react";
+import "../journey/journey.css";
 
 const Journey = () => {
   return (
-    <section id="journey">
-      <h5>Glimse About My</h5>
+    <section id="journey" className="container">
+      <h5>Glimpse About My</h5>
       <h2>Journey</h2>
-      
+
+      <div className="qualification__container">
+        <div className="journey__tabs">
+          <div className="qualification__button button--flex">
+            <i className="uil uil-graduation-cap qualification__icon"></i>{" "}
+            Education
+          </div>
+          <div className="qualification__button button--flex">
+            <i className="uil uil-briefcase-alt qualification__icon"></i>{" "}
+            Experience
+          </div>
+        </div>
+        <div className="qualification__content">
+          <div className="qualification__sections">
+            <div className="qualification_data">
+              <div>
+                <h3 className="qualification__title">Web Designer</h3>
+                <span className="qualification__subtitle">Test dummy data</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Art Director</h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Test dummy data
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - present
+                </div>
+              </div>
+            </div>
+            <div className="qualification_data">
+              <div>
+                <h3 className="qualification__title">Web Developer</h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Test dummy data
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"> Test dummy data</span>
+              </div>
+              <div>
+                <h3 className="qualification__title">UX Expert</h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Test dummy data
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - present
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="qualification__content">
+            <div className="qualification_data">
+              <div>
+                <h3 className="qualification__title">Web Designer</h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Test dummy data
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">Art Director</h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Test dummy data
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - present
+                </div>
+              </div>
+            </div>
+            <div className="qualification_data">
+              <div>
+                <h3 className="qualification__title">Web Developer</h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Test dummy data
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - present
+                </div>
+              </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification_data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">UX Expert</h3>
+                <span className="qualification__subtitle">
+                  {" "}
+                  Test dummy data
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2021 - present
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
